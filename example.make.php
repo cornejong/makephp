3d0b59288412d7e5a954077075d62778094ab192
Corné de Jong <5366568-cornedejong@users.noreply.gitlab.com>
2021-09-17 18:58:03 +0200

    added a beter make.php init example

---
+++ example.make.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This is a makephp example file.
+ * You'd place this file in your project's root directory
+ * 
+ * If you call 'makephp' without an argument the app will call
+ * the first function in this file.
+ * 
+ * otherwise the specified function will be called
+ */
+
+function main()
+{
+	/* Define the requirements for this function */
+	/* Either a single function name (as a string) */
+	/* Or an array of function names */
+    requires([
+		'setup',
+		'build',
+		'test',
+	]);
+}
+
+function setup() {}
+function build() {}
+function test() {}
+
+?>
